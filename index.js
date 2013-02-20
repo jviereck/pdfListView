9b863a02135376d57e1a8f0e3d35e967b731bc7b
Julian Viereck <julian.viereck@gmail.com>
2013-02-20 18:53:38 +0100

    Implement different scaling modes

--- index.js
+++ index.js
@@ -111,7 +111,10 @@ RenderController.prototype = {
 };
 
 var LAYOUT_SINGLE = 'layout_single';
+var SCALE_MODE_AUTO = 'scale_mode_auto';
 var SCALE_MODE_VALUE = 'scale_mode_value';
+var SCALE_MODE_FIT_WIDTH = 'scale_mode_fit_width';
+var SCALE_MODE_FIT_HEIGHT = 'scale_mode_fit_height';
 
 /**
  * Main view that holds the single pageContainer/pageViews of the pdfDoc.
@@ -119,7 +122,7 @@ var SCALE_MODE_VALUE = 'scale_mode_value';
 function ListView(pdfDoc, dom) {
     this.onScroll = this.onScroll.bind(this);
 
-    this.pdf = pdfDoc;
+    this.pdfDoc = pdfDoc;
     this.dom = dom;
 
     this.pageLayout = LAYOUT_SINGLE;
@@ -129,27 +132,30 @@ function ListView(pdfDoc, dom) {
     this.pageViews = [];
     this.containerViews = [];
 
-    // TODO: Handle multiple layout types here. For now, assume to have one page
-    // per pageContainer.
-    pdfDoc.pages.map(function(page) {
-        var pageView = new PageView(page, this);
-        this.pageViews.push(pageView);
-
-        var container = new PageContainerView(pageView);
-        container.setPageView(pageView, 0);
-        this.containerViews.push(container);
-
-        dom.appendChild(container.dom);
-    }, this);
-
+    this.assignPagesToContainer();
 
     this.onScroll();
     this.dom.addEventListener('scroll', this.onScroll);
 }
 
 ListView.prototype = {
+    assignPagesToContainer: function() {
+        // TODO: Handle multiple layout types here. For now, assume to have one page
+        // per pageContainer.
+        this.pdfDoc.pages.map(function(page) {
+            var pageView = new PageView(page, this);
+            this.pageViews.push(pageView);
+
+            var container = new PageContainerView(this);
+            container.setPageView(pageView, 0);
+            this.containerViews.push(container);
+
+            this.dom.appendChild(container.dom);
+        }, this);
+    },
+
     onScroll: function() {
-        // Cache these resutls to avoid dom access.
+        // Cache these results to avoid dom access.
         this.scrollTop = this.dom.scrollTop;
         this.scrollBottom = this.scrollTop + this.dom.clientHeight;
     },
@@ -158,6 +164,34 @@ ListView.prototype = {
         this.containerViews.forEach(function(containerView) {
             containerView.layout();
         });
+    },
+
+    // Calculates the new scale. Returns `true` if the scale changed.
+    calculateScale: function() {
+        var newScale = this.scale;
+        var oldScale = newScale;
+        var scaleMode = this.scaleMode;
+        if (scaleMode === SCALE_MODE_FIT_WIDTH || scaleMode === SCALE_MODE_AUTO) {
+            var offsetWidth = this.dom.offsetWidth;
+            var maxNormalWidth = 0;
+            this.containerViews.forEach(function(containerView) {
+                maxNormalWidth = Math.max(maxNormalWidth, containerView.normalWidth);
+            });
+            var scale = offsetWidth/maxNormalWidth;
+            if (scaleMode === SCALE_MODE_AUTO) {
+                scale = Math.min(1.0, scale);
+            }
+            newScale = scale;
+        } else if (scaleMode === SCALE_MODE_FIT_HEIGHT) {
+            var offsetHeight = this.dom.offsetHeight;
+            var maxNormalHeight = 0;
+            this.containerViews.forEach(function(containerView) {
+                maxNormalHeight = Math.max(maxNormalHeight, containerView.normalHeight);
+            });
+            newScale = offsetHeight/maxNormalHeight;
+        }
+        this.scale = newScale;
+        return newScale !== oldScale;
     },
 
     getPagesToRender: function() {
@@ -178,7 +212,9 @@ ListView.prototype = {
  * every pageContainerView holds two PageViews and is responsible to layout
  * them.
  */
-function PageContainerView(pageCount) {
+function PageContainerView(listView) {
+    this.listView = listView;
+
     var dom = this.dom = document.createElement('div');
     dom.className = 'pageContainer';
     this.pages = [];
@@ -199,12 +235,22 @@ PageContainerView.prototype = {
     },
 
     layout: function() {
-        var width = 0;
+        var scale = this.listView.scale;
+
+        var normalWidth = 0;
+        var normalHeight = 0;
+
         this.pages.forEach(function(pageView) {
             pageView.layout();
-            width += pageView.width;
+            normalWidth += pageView.normalWidth;
+            normalHeight = Math.max(pageView.normalHeight, normalHeight);
         });
-        this.dom.style.width = width + 'px';
+
+        this.normalWidth = normalWidth;
+        this.normalHeight = normalHeight;
+
+        this.dom.style.width = (normalWidth * scale) + 'px';
+        this.dom.style.height = (normalHeight * scale) + 'px';
     }
 };
 
@@ -224,6 +270,9 @@ function PageView(page, listView) {
     this.page = page;
     this.listView = listView;
     this.id = idCounter++;
+    this.number = this.page.number;
+
+    this.rotation = 0;
 
     this.isRendered = false;
     this.renderState = RenderingStates.INITIAL;
@@ -237,16 +286,29 @@ function PageView(page, listView) {
 PageView.prototype = {
     layout: function() {
         var scale = this.listView.scale;
-        var totalRotation = 0;
 
-        // TODO: Rotation
         var viewport = this.viewport =
-            this.page.pdfPage.getViewport(scale, totalRotation);
+            this.page.pdfPage.getViewport(scale, this.rotation);
+
+        this.normalWidth = viewport.width / scale;
+        this.normalHeight = viewport.height / scale;
+
+        // Only change the width/height property of the canvas if it really
+        // changed. Every assignment to the width/height property clears the
+        // content of the canvas.
+        var newWidth = Math.floor(viewport.width);
+        var newHeight = Math.floor(viewport.height);
+        if (this.canvas.width !== newWidth) {
+            this.canvas.width = newWidth;
+            this.resetRenderState();
+        }
+        if (this.canvas.height !== newHeight) {
+            this.canvas.height = newHeight;
+            this.resetRenderState();
+        }
 
         this.width = viewport.width;
         this.height = viewport.height;
-        this.canvas.width = Math.floor(this.width);
-        this.canvas.height = Math.floor(this.height);
     },
 
     isVisible: function() {
@@ -259,12 +321,13 @@ PageView.prototype = {
                 offsetTop <= listView.scrollBottom;
     },
 
-    render: function(renderController) {
-        return this.page.render(this, renderController);
+    resetRenderState: function() {
+        this.renderState = RenderingStates.INITIAL;
+        this.isRendered = false;
     },
 
-    get number() {
-        return this.page.number;
+    render: function(renderController) {
+        return this.page.render(this, renderController);
     },
 
     getCanvasContext: function() {
@@ -304,8 +367,9 @@ PDFPage.prototype = {
         // Not most important page to render ATM.
         if (renderController.pageToRender() !== pageView) return;
 
+        var viewport;
         if (renderContext = this.renderContextList[pageView.id]) {
-            var viewport = renderContext.viewport;
+            viewport = renderContext.viewport;
 
             // TODO: handle rotation
             if (viewport.height !== pageView.viewport.height ||
@@ -321,13 +385,21 @@ PDFPage.prototype = {
         }
 
         if (!renderContext) {
+            viewport = pageView.viewport;
             // No rendering data yet -> create a new renderContext and start
             // the rendering process.
             renderContext = {
               canvasContext: pageView.getCanvasContext(),
-              viewport: pageView.viewport,
+              viewport: viewport,
               // textLayer: textLayer,
               continueCallback: function pdfViewContinueCallback(cont) {
+                if (viewport.height !== pageView.viewport.height ||
+                    viewport.height !== pageView.viewport.height)
+                {
+                  // If the viewport changed while rendering, then stop here.
+                  return;
+                }
+
                 if (renderController.pageToRender() !== pageView) {
                   console.log('PAUSE', pageView.id);
                   renderContext.state = RenderingStates.PAUSED;
@@ -358,6 +430,13 @@ PDFPage.prototype = {
     }
 };
 
+function switchToAutoScale() {
+    listView.scaleMode = SCALE_MODE_AUTO;
+    listView.calculateScale();
+    listView.layout();
+    renderController.updateRenderList();
+}
+
 function boot() {
     var pdf = new Document('external/tracemonkey.pdf');
     pdf.initialized.then(function() {
@@ -374,6 +453,16 @@ function boot() {
         mainDiv.addEventListener('scroll', function() {
             // This will update the list AND start rendering if needed.
             renderController.updateRenderList();
+        });
+
+        window.addEventListener('resize', function() {
+            // Check if the scale changed due to the resizing.
+            if (listView.calculateScale()) {
+                // Update the layout and start rendering. Changing the layout
+                // of the PageView makes it rendering stop.
+                listView.layout();
+                renderController.updateRenderList();
+            }
         });
 
         window.listView = listView;
